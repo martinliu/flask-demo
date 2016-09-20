9b7b0cfcf81f233109442ea2de14b83459cbace7
martinliu <liuzh66@gmail.com>
2016-09-20 18:57:55 +0800

    update

--- src/app.py
+++ src/app.py
@@ -7,6 +7,6 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
 	redis.incr('hits')
-	return 'Hello world, I am working at Eloancn.com ! I have been here %s times.'  % redis.get('hits')
+	return 'Hello world, I am working at Eloancm ! I have been here %s times.'  % redis.get('hits')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
