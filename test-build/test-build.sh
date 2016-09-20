801cca3f0ecd17a93198de031e165ca15bf09c8e
martinliu <liuzh66@gmail.com>
2016-09-20 17:25:53 +0800

    loan

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#cd ${WORKSPACE}/src
-#docker build -t 192.168.99.50:5000/python-redis-demo:b${BUILD_NUMBER} .
-#docker push 192.168.99.50:5000/python-redis-demo:b${BUILD_NUMBER}
-#cd ${WORKSPACE}/test-build
-sed -i '' 's/MYBUILD/'${BUILD_NUMBER}'/g' docker-compose.yml
-sed -i '' 's/MYPORT/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
-#chmod 777 ./rancher-compose
-#./rancher-compose --url http://192.168.99.1:8080 --access-key FE362F3CA9E9BD8289E2 --secret-key yD2Uc789cCPzeEQySA1Q2UNMFKrSeq2SgzGYvAyp -p python-redis-demo-build${BUILD_NUMBER} up -d
+cd ${WORKSPACE}/src
+docker build -t 121.199.10.99:81/python-redis-demo:b${BUILD_NUMBER} .
+docker push 121.199.10.99:81/python-redis-demo:b${BUILD_NUMBER}
+cd ${WORKSPACE}/test-build
+sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
+sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
+chmod 777 ./rancher-compose
+rancher-compose --url http://192.168.2.93:8080 --access-key 15A33DF17A44FA7DD156 --secret-key sdtCMjkpquoSLUfB3KzQdWwKhNaMF5WXhFV7b3co -p python-redis-demo-build${BUILD_NUMBER} up -d
 #./rancher-compose --url http://10.0.0.5:8080 --access-key CA23527D9BE1E5855619 --secret-key GF6Q1vMsimqY8MHp6t17eqoZXcbQ8VEBcjU11z7H -p python-redis-demo-build27 up --pull -d --upgrade pyapp
 # --confirm-upgrade
